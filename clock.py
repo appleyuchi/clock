3d5155551603067eef65b8d481f5f82de2d4984b
appleyuchi <appleyuchi@163.com>
2021-01-20 15:43:12 +0800

    已经经过部署验证

--- clock.py
+++ clock.py
@@ -1,8 +1,7 @@
 from flask import  Flask,render_template
-app = Flask(__name__)
-
-@app.route('/')
+app = Flask(__name__,static_folder='', static_url_path='')
+@app.route('/clock')
 def adddemo1():
     return render_template('hello.html') 
 if __name__ == '__main__':
-    app.run(port=10073,debug=False)
+    app.run(port=10073,debug=True)
